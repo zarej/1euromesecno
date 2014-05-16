42bed3d2c42b055f15270dbec2caa768715d1798
Marko Kacanski <marko.kacanski@gmail.com>
2014-05-17 00:36:28 +0200

    created package for beneficiary retrieval

---
+++ 1euromesecno-sdk/src/rs/hakaton/euromesecno/sdk/beneficiaryservice/BenficiaryService.java
@@ -0,0 +1,7 @@
+package rs.hakaton.euromesecno.sdk.beneficiaryservice;
+
+import rs.hakaton.euromesecno.sdk.webservice.WebService;
+
+public class BenficiaryService extends WebService {
+
+}
---
+++ 1euromesecno-sdk/src/rs/hakaton/euromesecno/sdk/beneficiaryservice/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author marko
+ *
+ */
+package rs.hakaton.euromesecno.sdk.beneficiaryservice;
